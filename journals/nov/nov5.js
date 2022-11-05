66d898411037cf35ee6da5498a397c0c5ed107b0
CAS-CAS-CAS <247Adventurer@gmail.com>
2022-11-05 13:46:47 -0700

    feelin good

---
+++ journals/nov/nov5.js
@@ -0,0 +1,15 @@
+let nums = [2, 3, 5, 7, 9]
+
+let target = 7
+
+function binarySearch(goods){
+    if(!Array.isArray(goods))
+    {
+        throw Error
+    }
+
+    let index = nums.length / 2
+    
+}
+
+binarySearch(nums)
